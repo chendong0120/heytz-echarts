2e66289df2d74fdd2efd2667c32e08c6966f66cc
陈东东 <kevinchen@infapp.com>
2016-04-25 16:28:39 +0800

    更新版本，添加readme

---
+++ README.md
@@ -0,0 +1,4 @@
+请参考:
+    http://echarts.baidu.com/
+
+版本 3.0
--- package.js
+++ package.js
@@ -1,15 +1,20 @@
 Package.describe({
-    name: 'hetyz:echarts',
-    version: '0.3.0',
-    // Brief, one-line summary of the package.
-    summary: '',
-    // URL to the Git repository containing the source code for this package.
-    git: '',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
-    documentation: 'README.md'
+  name: 'kevinchen:echarts',
+  version: '0.3.0',
+  summary: 'baidu echarts',
+  git: 'https://chendong0120@github.com/chendong0120/heytz-echarts.git',
+  documentation: 'README.md'
 });
-Package.onUse(function (api) {
-    api.versionsFrom('1.2');
-    api.addFiles('src/echarts.min.js', ['web.browser', 'web.cordova']);
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('src/echarts.min.js', ['web.browser', 'web.cordova']);
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('kevinchen:echarts');
+  api.addFiles('echarts-tests.js');
 });
