40dba88ea215bf1cbc93794e017013aa87910b0f
陈东东 <kevinchen@infapp.com>
2016-04-25 17:57:36 +0800

    修复bug

--- package.js
+++ package.js
@@ -1,20 +1,12 @@
 Package.describe({
-  name: 'kevinchen:echarts',
-  version: '0.3.0',
-  summary: 'baidu echarts',
-  git: 'https://chendong0120@github.com/chendong0120/heytz-echarts.git',
-  documentation: 'README.md'
+    name: 'kevinchen:echarts',
+    version: '0.3.1',
+    summary: 'baidu echarts',
+    git: 'https://github.com/chendong0120/heytz-echarts.git',
+    documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('src/echarts.min.js', ['web.browser', 'web.cordova']);
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('kevinchen:echarts');
-  api.addFiles('echarts-tests.js');
+Package.onUse(function (api) {
+    api.versionsFrom('1.2.1');
+    api.addFiles('src/echarts.min.js', 'client', {bare: true});
 });
