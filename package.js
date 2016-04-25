b3bae217e76afc5b99150ada2b6c19d1ab7786fb
陈东东 <kevinchen@infapp.com>
2016-04-25 15:31:56 +0800

    升级 echarts 到 3.0

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+    name: 'hetyz:echarts',
+    version: '0.3.0',
+    // Brief, one-line summary of the package.
+    summary: '',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+Package.onUse(function (api) {
+    api.versionsFrom('1.2');
+    api.addFiles('src/echarts.min.js', ['web.browser', 'web.cordova']);
+});
